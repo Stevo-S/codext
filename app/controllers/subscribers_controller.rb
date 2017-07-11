b4868d29d3fbde01d578f24a0c3d024a1cc03059
Steve <steve@smartfame.co.ke>
2017-07-11 20:43:38 +0300

    Show only the day's subscribers by default in the index view

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -3,6 +3,8 @@ class SubscribersController < ApplicationController
 	page = params[:page]
 	per_page = params[:per_page]
 	@q = Subscriber.ransack(params[:q])
+	@q.last_subscribed_at_gteq = Date.today.midnight unless params[:q]
+	@q.last_subscribed_at_lteq = Date.tomorrow.midnight unless params[:q]
 	@q.sorts = 'updated_at desc'
 	@subscribers = @q.result(distinct: true).page params[:page]
     end
