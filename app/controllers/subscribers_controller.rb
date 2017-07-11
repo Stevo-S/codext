596f715ab2140d374f8dff7b0bee8d119465a6a4
Steve <steve@smartfame.co.ke>
2017-07-11 23:34:36 +0300

    Make search parameters 'sticky'

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,8 +4,12 @@ class SubscribersController < ApplicationController
 	per_page = params[:per_page]
 	@q = Subscriber.ransack(params[:q])
 	@q.last_subscribed_at_gteq = Date.today.midnight unless params[:q]
-	@q.last_subscribed_at_lteq = Date.tomorrow.midnight unless params[:q]
 	@q.sorts = 'updated_at desc'
 	@subscribers = @q.result(distinct: true).page params[:page]
+
+	if params[:q].nil? then
+	    params[:q] = { last_subscribed_at_gteq: Date.today.midnight.strftime('%FT%T'),
+	   	last_subscribed_at_lteq: Date.tomorrow.strftime('%FT%T') }
+	end
     end
 end
