4b98e5b6e2ac3dde5b6a0c600657d406c6b6f5af
Steve <steve@smartfame.co.ke>
2017-09-05 03:40:47 +0300

    Fix subscriber Index view error

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -9,13 +9,13 @@ class SubscribersController < ApplicationController
 	per_page = params[:per_page]
 	@q = Subscriber.ransack(params[:q])
 	@q.last_subscribed_at_gteq = Date.today.midnight unless params[:q]
-	@q.active = = true unless params[:q]
+	@q.active = true unless params[:q]
 	@q.sorts = 'updated_at desc'
 	@subscribers = @q.result(distinct: true).accessible_by(current_ability).page params[:page]
 
 	if params[:q].nil? then
 	    params[:q] = { last_subscribed_at_gteq: Date.today.midnight.strftime('%FT%T'),
-	   	last_subscribed_at_lteq: Date.tomorrow.strftime('%FT%T') }
+	   	last_subscribed_at_lteq: Date.tomorrow.strftime('%FT%T'), active_eq: true }
 	end
     end
 end
